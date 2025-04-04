ea28c784a703f11e30b5694347ba1fd1bb67e0ae
Rodrigo-Ameixa <86132551+Rodrigo-Ameixa@users.noreply.github.com>
2025-04-04 16:47:12 +0100

    Initial commit

---
+++ .devcontainer/postStartCommand.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+chmod +x jupyter.sh 
+sudo nohup bash -c "JUPYTER_ENABLE_LAB=yes GRANT_SUDO=yes start.sh jupyter lab --ServerApp.token='' --port=8888 --no-browser &"
+
+echo "Close terminal and reopen to get correct shell."
